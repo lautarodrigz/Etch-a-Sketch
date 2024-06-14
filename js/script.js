eb24abf95b8405cc8128034c63fe360097bfd6ab
lautaro <lautirod12@gmail.com>
2024-06-14 19:30:36 -0300

    Make squares change color when you hover over them

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ for (let i = 0; i < 256; i++) {
     container.appendChild(square);
     squaresArray.push(square);
 }
-// squaresArray.forEach(square => {
-//     square.addEventListener("mouseover", () => {
-//         square.classList.add("active");
-//     });
-// });
+squaresArray.forEach(square => {
+    square.addEventListener("mouseover", () => {
+        square.classList.add("active");
+    });
+});
