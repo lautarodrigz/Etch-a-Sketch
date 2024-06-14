16cd414cf27f9813d0e2ee4afa6ac9087215b224
lautaro <lautirod12@gmail.com>
2024-06-14 19:29:31 -0300

    Create 16x16 grid inside container

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector(".container");
+const squaresArray = [];
+for (let i = 0; i < 256; i++) {
+    const square = document.createElement("div");
+    square.className = "square";
+    container.appendChild(square);
+    squaresArray.push(square);
+}
+// squaresArray.forEach(square => {
+//     square.addEventListener("mouseover", () => {
+//         square.classList.add("active");
+//     });
+// });
