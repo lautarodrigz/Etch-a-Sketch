cd493fc49524ef95807ca1671685d1a73f21c149
lautaro <lautirod12@gmail.com>
2024-06-17 20:55:31 -0300

    Let the user pick the grid size

--- js/script.js
+++ js/script.js
@@ -1,13 +1,31 @@
+const popUpButton = document.createElement("button");
+popUpButton.textContent = "Select the number of squares per side";
 const container = document.querySelector(".container");
 const squaresArray = [];
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement("div");
-    square.className = "square";
-    container.appendChild(square);
-    squaresArray.push(square);
-}
-squaresArray.forEach(square => {
-    square.addEventListener("mouseover", () => {
-        square.classList.add("active");
+let numberOfSquares;
+document.body.insertBefore(popUpButton, container)
+
+popUpButton.addEventListener("click", () => {
+    numberOfSquares = +prompt("Insert number of squares per side", "16");
+    console.log(numberOfSquares ** 2);
+    for (let i = 0; i < numberOfSquares ** 2; i++) {
+        const square = document.createElement("div");
+        square.className = "square";
+        square.style.setProperty("width", `calc(100%/${numberOfSquares})`);
+        square.style.setProperty("height", `calc(100%/${numberOfSquares})`);
+        container.appendChild(square);
+        squaresArray.push(square);
+    }
+    squaresArray.forEach(square => {
+        square.addEventListener("mouseover", () => {
+            square.classList.add("active");
+        });
     });
 });
+
+
+
+
+
+
+
